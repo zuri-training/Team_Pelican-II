24697cf84f577f0211e0abc2c76eca6ada87adde
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-12-15 22:20:06 +0100

    User CRUD implemented

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db.config');
+const { notFound, errorHandler } = require('./middleware/customAPIError');
+const userRouter = require('./routes/user.routes');
 
 
 //Get environmental variables
@@ -10,8 +12,11 @@ const DBURL = process.env.DBURL;
 //create app
 const app = express();
 app.use(express.json());
+app.use('/api/v1/user', userRouter);
 
-
+//Error handlers
+app.use('*', notFound)
+app.use(errorHandler)
 const startServer = async() =>{
 await connectDB(DBURL);
 app.listen(PORT, () => {
---
+++ server/controller/user.controller.js
@@ -0,0 +1,92 @@
+const {APIError} = require("../middleware/customAPIError"); 
+const userModel = require('../model/user.model');
+const bcrypt = require('bcrypt');
+
+const createAccount = async(req, res, next) => {
+    const {email, password} = req.body;
+    if (!email || !password)
+        return next(APIError.badRequest("Please Supply all the missing field!"));
+    
+    try {
+        const userExist = await userModel.findOne({email});
+        if (userExist)
+            return next(APIError.badRequest("Supplied email already exist, login to continue"));
+        if(!userExist){
+            const hashedPwd = await bcrypt.hash(password, 10);
+            const user = await userModel.create({email, password: hashedPwd})
+        if (user)
+            res.status(201).json({
+                success: true,
+                message: "User account created successfully!",
+                userData: user
+            }) 
+        }
+       
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateAccount = async(req, res, next) => {
+const {id} = req.body;
+if (!id)
+    return next(APIError.badRequest("Please supply your User ID"));
+try {
+    const findUser = await userModel.findById(id);
+if (!findUser)
+    return next(APIError.notFound("User with supplied ID does not exists"));
+const newUser = await userModel.findByIdAndUpdate(id, req.body);
+res.status(200).json({
+    success: true,
+    message: "User updated successfully",
+    updatedUser: newUser
+})
+} catch (error) {
+    next(error)
+}
+
+}
+
+const getUserAccount = async(req, res, next) =>{
+    const {id} = req.params;
+    if (!id)
+        return next(APIError.badRequest("Please supply your User ID"));
+        try {
+            const findUser = await userModel.findById(id);
+        if (!findUser)
+            return next(APIError.notFound("User with supplied ID does not exists"));
+        res.status(200).json({
+            success: true,
+            message: "User fetched successfully!",
+            updatedUser: findUser
+        })
+        } catch (error) {
+            next(error)
+        }
+}
+
+const deleteAccount = async(req, res, next) =>{
+    const {id} = req.params;
+    if (!id)
+        return next(APIError.badRequest("Please supply your User ID"));
+        try {
+            const findUser = await userModel.findById(id);
+        if (!findUser)
+            return next(APIError.notFound("User with supplied ID does not exists"));
+        deletedUser = await userModel.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: `User with Id ${id} has been deleted successfully!`,
+            deletedData: deletedUser
+        })
+        } catch (error) {
+            next(error)
+        }
+}
+
+module.exports = {
+    createAccount,
+    updateAccount,
+    deleteAccount,
+    getUserAccount
+}
